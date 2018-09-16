d12e609f158f5f6b2d6a411373d2e2db31ac58a9
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-09-16 11:48:18 -0700

    game functioning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 //VARIABLES
 var topics = ["Joan Crawford", "Bette Davis", "Mae West", "Marlene Dietrich", "Rita Hayworth", "Greta Garbo"];
 
+
 //FUNCTIONS
 function renderButtons(){
     $("#buttons-view").empty();
@@ -14,25 +15,49 @@ function renderButtons(){
 }
 
 // Function for dumping the JSON content for each button into the div
-function displayGifs() {
+function giphyCall() {
     var search = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=caRg3WZm2q4HA0mBSohCihWnaI5DcBMv&limit=10";
         
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
-        $("#gif-view").empty();
-        for(var gif of response.data){
-            var gifDiv = $("<div class='gif'>");
-            
-            // Retrieving the URL for the image
-            var imgURL = gif.images.fixed_height_still.url;
-            var image = $("<img>").attr("src", imgURL);
-            gifDiv.append(image);
-            $("#gif-view").append(gifDiv);
-        }
-    });
+    }).then(createGif);
+}
+
+function createGif(x){
+    var imgURL;
+    var gifURL;
+    $("#gif-view").empty();
+    for(var gif of x.data){
+        var gifDiv = $("<div class='gif'>");
+        
+        // Retrieving the URLs for the image
+        imgURL = gif.images.fixed_height_still.url;
+        gifURL = gif.images.fixed_height.url;
+        rating = gif.rating;
+
+        //set image attributes and append image to page
+        var image = $("<img>").attr("src", imgURL);
+        image.attr("data-state", "still");
+        image.attr("data-still", imgURL);
+        image.attr("data-animate", gifURL);
+        gifDiv.append(image);
+        gifDiv.prepend("<br>Rating: " + rating + "<br>");
+        $("#gif-view").append(gifDiv);
+
+        //on click toggle state
+        $(image).on("click", function() {
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
+    }
 }
 
 //EVENTS
@@ -45,17 +70,6 @@ $("#submit-button").on("click", function(event) {
 });
 
 // Display gifs when one of the topic buttons is clicked
-$(document).on("click", ".topic-button", displayGifs);
-
-$(".gif").on("click", function() {
-    var state = $(this).attr("data-state");
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-    } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-    }
-  });
+$(document).on("click", ".topic-button", giphyCall);
 
 renderButtons();
