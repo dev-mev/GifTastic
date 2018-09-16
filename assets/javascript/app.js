56caaca6ba9ed53e3ad87a7cfc82879799410cf3
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-09-15 22:38:47 -0700

    add buttons and pull gifs from Giphy API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,61 @@
+//VARIABLES
 var topics = ["Joan Crawford", "Bette Davis", "Mae West", "Marlene Dietrich", "Rita Hayworth", "Greta Garbo"];
 
-  // Function for dumping the JSON content for each button into the div
-  function displayGifInfo() {
+//FUNCTIONS
+function renderButtons(){
+    $("#buttons-view").empty();
+    for(topic of topics){
+        var a = $("<button>");
+        a.addClass("topic-button");
+        a.attr("data-name", topic);
+        a.text(topic);
+        $("#buttons-view").append(a);
+    }
+}
 
-    var search = topics[0]; //$(this).attr("data-name");
+// Function for dumping the JSON content for each button into the div
+function displayGifs() {
+    var search = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=caRg3WZm2q4HA0mBSohCihWnaI5DcBMv&limit=10";
-        console.log(queryURL);
-
+        
     $.ajax({
-      url: queryURL,
-      method: "GET"
+        url: queryURL,
+        method: "GET"
     }).then(function(response) {
-      $("#gif-view").text(JSON.stringify(response));
-      console.log(response);
+        $("#gif-view").empty();
+        for(var gif of response.data){
+            var gifDiv = $("<div class='gif'>");
+            
+            // Retrieving the URL for the image
+            var imgURL = gif.images.fixed_height_still.url;
+            var image = $("<img>").attr("src", imgURL);
+            gifDiv.append(image);
+            $("#gif-view").append(gifDiv);
+        }
     });
-  }
+}
 
-  displayGifInfo();
+//EVENTS
+$("#submit-button").on("click", function(event) {
+    event.preventDefault();
+    var addTopic = $("#add-leading-lady").val().trim();
+    topics.push(addTopic);
+    $("#add-leading-lady").val("");
+    renderButtons();
+});
+
+// Display gifs when one of the topic buttons is clicked
+$(document).on("click", ".topic-button", displayGifs);
+
+$(".gif").on("click", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
+renderButtons();
