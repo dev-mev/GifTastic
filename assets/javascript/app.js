b12ce18e9b1df1390762148ab37b5a2ca0d42cb5
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-09-16 19:13:25 -0700

    complete functioning game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,38 @@
-//VARIABLES
-var topics = ["Joan Crawford", "Bette Davis", "Mae West", "Marlene Dietrich", "Rita Hayworth", "Greta Garbo"];
-
-
 //FUNCTIONS
+//function to create buttons that stay on the screen
 function renderButtons(){
-    $("#buttons-view").empty();
-    for(topic of topics){
-        var a = $("<button>");
-        a.addClass("topic-button");
-        a.attr("data-name", topic);
-        a.text(topic);
-        $("#buttons-view").append(a);
+    var topics = [
+        "Joan Crawford",
+        "Bette Davis",
+        "Mae West",
+        "Marlene Dietrich",
+        "Rita Hayworth",
+        "Greta Garbo",
+        "Ava Gardner",
+        "Lana Turner"
+    ];
+
+    for(var topic of topics){
+        renderNewButton(topic);
     }
 }
 
-// Function for dumping the JSON content for each button into the div
-function giphyCall() {
+//function to make new button
+function renderNewButton(topic){
+    var topicButton = $("<button class='topic-button lead'>")
+        .attr("data-name", topic)
+        .text(topic)
+        .click(function(){
+            $(".topic-button").removeClass("active");
+            $(this).addClass("active");
+    });
+    $("#buttons-view").append(topicButton);
+}
+
+// Function for retrieving JSON content from the GIPHY API
+function callGiphy() {
     var search = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=caRg3WZm2q4HA0mBSohCihWnaI5DcBMv&limit=10";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + encodeURIComponent(search) + "&api_key=caRg3WZm2q4HA0mBSohCihWnaI5DcBMv&limit=10";
         
     $.ajax({
         url: queryURL,
@@ -25,25 +40,27 @@ function giphyCall() {
     }).then(createGif);
 }
 
-function createGif(x){
+//Function that takes the response from the API call and appends the gifs to #gif-view on the page
+function createGif(response){
     var imgURL;
     var gifURL;
     $("#gif-view").empty();
-    for(var gif of x.data){
+    for(var gif of response.data){
         var gifDiv = $("<div class='gif'>");
+        var ratingDiv = $("<div class='rating lead'>");
         
         // Retrieving the URLs for the image
         imgURL = gif.images.fixed_height_still.url;
         gifURL = gif.images.fixed_height.url;
-        rating = gif.rating;
+        var rating = ratingDiv.text("Rating: " + gif.rating);
 
         //set image attributes and append image to page
         var image = $("<img>").attr("src", imgURL);
         image.attr("data-state", "still");
         image.attr("data-still", imgURL);
         image.attr("data-animate", gifURL);
+        gifDiv.append(rating);
         gifDiv.append(image);
-        gifDiv.prepend("<br>Rating: " + rating + "<br>");
         $("#gif-view").append(gifDiv);
 
         //on click toggle state
@@ -61,15 +78,20 @@ function createGif(x){
 }
 
 //EVENTS
+//takes input from user and passes it into function renderNewButton or if nothing was entered displays an alert
 $("#submit-button").on("click", function(event) {
-    event.preventDefault();
-    var addTopic = $("#add-leading-lady").val().trim();
-    topics.push(addTopic);
-    $("#add-leading-lady").val("");
-    renderButtons();
+    if($("#add-dame").val().trim() != ""){
+        event.preventDefault();
+        var addTopic = $("#add-dame").val().trim();
+        $("#add-dame").val("");
+        renderNewButton(addTopic);
+    }
+    else{
+        alert("Please enter a name.")
+    }
 });
 
 // Display gifs when one of the topic buttons is clicked
-$(document).on("click", ".topic-button", giphyCall);
+$(document).on("click", ".topic-button", callGiphy);
 
 renderButtons();
